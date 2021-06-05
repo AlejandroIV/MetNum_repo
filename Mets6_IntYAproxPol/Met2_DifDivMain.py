83b3829ea15fcab4eeec5bec1e1192375859b0ff
Alejandro <AlejandroDR_IV@Outlook.com>
2021-06-05 18:28:04 -0500

    Metodos Numericos en Python3

--- Mets6_IntYAproxPol/Met2_DifDivMain.py
+++ Mets6_IntYAproxPol/Met2_DifDivMain.py
@@ -1,6 +1,6 @@
 """Modulo que contiene el codigo para construir el Polinomio Interpolante de Newton"""
 
-from sage.all import *
+from sage.all import SR, sage, round
 import sys
 import numpy as np
 from Mets6_IntYAproxPol.Mods_Preparar_Metodos.Preparar_Programa import LLenar_Matriz_Datos, OpcionesDifDiv
@@ -9,32 +9,20 @@ def Interpolacion_Diferencias_Divididas(nombre):
     """Funcion que construira el Polinomio Interpolante de Newton"""
     # Primero llena una matriz con los datos contenidos en el documento de texto
     matDatos = LLenar_Matriz_Datos(nombre)
+    # Une la matriz 'matDatos' con otra matriz de ceros de orden n x n - 1, donde n es el numero de datos
+    matDatos = np.copy(np.append(matDatos, np.zeros((matDatos.shape[0], (matDatos.shape[0] - 1)), dtype = 'f'), axis = 1))
 
-    # Crea un vector columna auxiliar para ir guardando lo resultados que se almacenaran en a tabla
-    vectResultadosAux = np.empty((matDatos.shape[0], 1), dtype = 'f')
-    # Contador que se usara para saber que columna de 'matDatos' se va a utilizar para calcular el numerador
-    cont1 = 1
     # Bucle que se repetira hasta calcular la ultima diferencia dividida posible
-    for diferenciaDividida in range(matDatos.shape[0] - 1):
-        # Contador que se va a usar para saber que fila de 'matDatos' se va a utilizar para calcular el sustraendo del denominador
-        cont2 = 0  # Este contador tambien se va a utilizar para indexar a 'vectResultadosAux'
+    # Este contador se utilizara para indexar los valores de las columnas de 'matDatos' [Burden p. 124]
+    for cont1 in range(1, matDatos.shape[0]):
+        #  Este contador se utilizara para indexar los valores de las filas de 'matDatos'
+        cont2 = 0
         # Bucle que calcula los elementos de la tabla de diferencias divididas
         for elemRengl in range(cont1, matDatos.shape[0]):
             # La variable 'elemRengl' va a indexar la fila del valor de x que se va a usar en el minuendo (primer elemento de la resta)
-            resultado = (matDatos[(cont2 + 1), cont1] - matDatos[(cont2), cont1]) / (matDatos[elemRengl, 0] - matDatos[cont2, 0])
-            vectResultadosAux[cont2, 0] = resultado
+            matDatos[cont2, (cont1 + 1)] = (matDatos[(cont2 + 1), cont1] - matDatos[cont2, cont1]) / (matDatos[elemRengl, 0] - matDatos[cont2, 0])
             cont2 += 1
 
-        # Bucle que agregara ceros al resto de filas del vector
-        for ceros in range(cont1):
-            vectResultadosAux[cont2, 0] = 0
-            cont2 += 1
-
-        cont1 += 1
-
-        # Agrega la columna que se calculo a la matriz
-        matDatos = np.append(matDatos, vectResultadosAux, axis = 1)
-        
     np.set_printoptions(precision = 6, suppress = True)
 
     print(f"\n{matDatos}\n")
@@ -50,17 +38,12 @@ def Interpolacion_Diferencias_Divididas(nombre):
             print("Entrada invalida")
     
     # Crea la variable que contendra el polinomio en formato'str'
-    polinomio = ''
+    polinomio = f"({round(matDatos[0, 1], 8)})+"  # Primero agrega el primer termino del polinomio (constante)
     # Lista que contendra los terminos lineales
-    terLin = []
+    terLin = [f"(x - {round(matDatos[0, 0], 6)})"]  # Almacena el primer termino lineal en la lista
     # Bucle que construye el polinomio usando como coeficientes los elementos de la primera fila de la matriz
-    for coeficientes in range(num + 1):
+    for coeficientes in range(1, num + 1):
         polinomio += f"({round(matDatos[0, (coeficientes + 1)], 8)}"
-        if coeficientes == 0:
-            polinomio += ")+"
-            # Agrega un nuevo termino a la lista
-            terLin.append(f"(x - {round(matDatos[coeficientes, 0], 6)})")
-            continue
         # Bucle que agregara los terminos lineales al polinomio
         for terminosLineales in terLin:
             polinomio += f" * {terminosLineales}"
@@ -69,11 +52,10 @@ def Interpolacion_Diferencias_Divididas(nombre):
         # Agrega un nuevo termino a la lista de terminos lineales
         terLin.append(f"(x - {round(matDatos[coeficientes, 0], 6)})")
 
-        cont1 += 1  # No se usa BORRAR
-
     # Elimina el ultimo signo '+' que esta en la cadena que contiene el polinomio
     polinomio = polinomio[:(len(polinomio) - 1)]
 
+    # Pide al usuario que elija una opcion
     opcion = OpcionesDifDiv()
 
     if opcion == 1:
