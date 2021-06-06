c59ddf54e8bfe03ace7c3d7b5dfa66f3d475ee21
Alejandro <AlejandroDR_IV@Outlook.com>
2021-06-05 22:09:49 -0500

    Metodos Numericos en Python3

--- Mets6_IntYAproxPol/Met3_NewAdeYAtrMain.py
+++ Mets6_IntYAproxPol/Met3_NewAdeYAtrMain.py
@@ -1,60 +1,43 @@
 """Modulo que contiene el codigo para aplicar las Formulas de Interpolacion de Newton: hacia adelante y hacia atras"""
 
-from sage.all import *
+from sage.all import SR, sage, round
 import sys
 import numpy as np
-from Mets6_IntYAproxPol.Mods_Preparar_Metodos.Preparar_Programa import LLenar_Matriz_Datos, OpcionesNet, Modificar_Matriz
+from Mods_Preparar_Metodos.Preparar_Programa import LLenar_Matriz_Datos, OpcionesNet, Modificar_Matriz_DifRegr
 
 def Interpolacion_Newton_Adelante_Atras(nombre):
-    """Funcion que construira la tabla de las diferencias para aplciar la Formula de Interpolacion de Newton"""
+    """Funcion que construira la tabla de las diferencias para aplicar la Formula de Interpolacion de Newton"""
     # Primero llena una matriz con los datos contenidos en el documento de texto
     matDatos = LLenar_Matriz_Datos(nombre)
+    # Une la matriz 'matDatos' con otra matriz de ceros de orden n x n - 1, donde n es el numero de datos
+    matDatos = np.copy(np.append(matDatos, np.zeros((matDatos.shape[0], (matDatos.shape[0] - 1)), dtype = 'f'), axis = 1))
 
-    # Crea un vector columna auxiliar para ir guardando lo resultados que se almacenaran en a tabla
-    vectResultadosAux = np.empty((matDatos.shape[0], 1), dtype = 'f')
-    # Contador que se usara para saber que columna de 'matDatos' se va a utilizar para calcular el numerador
-    cont1 = 1
     # Bucle que se repetira hasta calcular la ultima diferencia posible
-    for diferencia in range(matDatos.shape[0] - 1):
-        cont2 = 0  # Contador que se va a usar para indexar a 'vectResultadosAux'
+    # Este contador se utilizara para indexar los valores de las columnas de 'matDatos'
+    for cont1 in range(1, matDatos.shape[0]):
+        #  Este contador se utilizara para indexar los valores de las filas de 'matDatos'
+        cont2 = 0
         # Bucle que calcula los elementos de la tabla de diferencias
-        for iteraciones in range(cont1, matDatos.shape[0]):
-            # La variable 'iteraciones' se va a utilizar para idexar las filas que se van a usar de la matriz 'matDatos'
-            resultado = matDatos[(cont2 + 1), cont1] - matDatos[cont2, cont1]
-            vectResultadosAux[cont2, 0] = resultado
+        for iteracion in range(cont1, matDatos.shape[0]):
+            # La variable 'elemRengl' va a indexar la fila del valor de x que se va a usar en el minuendo (primer elemento de la resta)
+            matDatos[cont2, (cont1 + 1)] = matDatos[(cont2 + 1), cont1] - matDatos[cont2, cont1]
             cont2 += 1
-
-        # Bucle que agregara ceros al resto de filas del vector
-        for ceros in range(cont1):
-            vectResultadosAux[cont2, 0] = 0
-            cont2 += 1
-
-        cont1 += 1
-
-        # Agrega la columna que se calculo a la matriz
-        matDatos = np.append(matDatos, vectResultadosAux, axis = 1)
 
     np.set_printoptions(precision = 6, suppress = True)
 
-    # Pide al usuario que elija aplicar el metodo de newton hacia adelante o ahacia atras
+    # Pide al usuario que elija una opcion
     opcion = OpcionesNet()
 
     # Si el usuario elije aplicar la interpolacion de newton hacia adelante entonces necesitamos indexar los valores de la primera fila de la matriz 'matDatos'
     if opcion == 1:
         fila = 0
-        # Calcula 's'
-        x = float(input("\nIngresa una abscisa: "))  # Pide al usuario una abscisa
-        s = (x - matDatos[fila, 0]) / (matDatos[1, 0] - matDatos[0, 0])
-        print(f"\n{matDatos}\n")
 
     # Si el usuario elije aplicar la interpolacion de newton hacia atras entonces necesitamos indexar los valores de la ultima fila de la matriz 'matDatos'
     else:
         fila = matDatos.shape[0] - 1
-        # Calcula 's'
-        x = float(input("\nIngresa una abscisa: "))  # Pide al usuario una abscisa
-        s = (x - matDatos[fila, 0]) / (matDatos[1, 0] - matDatos[0, 0])
-        matDatos = Modificar_Matriz(matDatos)
-        print(f"\n{matDatos}\n")
+        matDatos = Modificar_Matriz_DifRegr(matDatos)
+
+    print(f"\n{matDatos}\n")
 
     # Pide al usuario el grado del polinomio
     while True:
@@ -66,31 +49,29 @@ def Interpolacion_Newton_Adelante_Atras(nombre):
         except:
             print("Entrada invalida")
 
+    # Calcula 's'
+    x = float(input("\nIngresa una abscisa: "))  # Pide al usuario una abscisa
+    s = (x - matDatos[fila, 0]) / (matDatos[1, 0] - matDatos[0, 0])
+
     # Crea la variable que contendra el polinomio en formato'str'
-    polinomio = ''
+    polinomio = f"({round(matDatos[fila, 1], 8)})+"  # Primero agrega el primer termino del polinomio (constante)
     # Lista que contendra los terminos lineales que se multiplicaran por las deltas
-    terminos = []
+    terminos = ['s']   # Almacena el elemento 's' en la lista
     # Bucle que recorrera las columnas de la fila con la que se desee trabajar (la primera o la ultima)
-    for deltas in range(num + 1):
+    for deltas in range(1, num + 1):
         polinomio += f"(({round(matDatos[fila, (deltas + 1)], 8)}"
-        if deltas == 0:
-            polinomio += "))+"
-            # Agrega un nuevo termino a la lista
-            terminos.append('s')
-            continue
         # Bucle que agregara los terminos
         for termino in terminos:
             polinomio += f" * {termino}"
         polinomio += f") / (factorial({deltas})))+"
 
-        # Si el usuario eligio aplicar la interpolacion de newton hacia adelante se agregan terminos como: (s - n)
+        # Agrega un nuevo termino a la lista
+        # Si el usuario eligio aplicar la interpolacion de newton hacia adelante se agregan terminos de la forma: (s - n) [Burden p. 126]
         if opcion == 1:
-            # Agrega un nuevo termino a la lista
             terminos.append(f"(s - {deltas})")
 
-        # Si el usuario eligio plicar la interpolacion de newton hacia atras se agregan terminos como: (s + n)
+        # Si el usuario eligio plicar la interpolacion de newton hacia atras se agregan terminos de la forma: (s + n) [Burden p. 127]
         else:
-            # Agrega un nuevo termino
             terminos.append(f"(s + {deltas})")
 
     # Elimina el ultimo signo '+' que esta en la cadena que contiene el polinomio
